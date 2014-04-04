278f2fe6b615b0a044edb3ce699955841b4a7d8b
WackyWalrus <mcculloughjchris@gmail.com>
2014-04-04 14:14:46 -0400

    Upgraded
    Made it use event listeners instead. Ability to add and remove event
    listeners quickly.

--- Binder.js
+++ Binder.js
@@ -1,18 +1,16 @@
 var Binder = function(){
-	// mouse events
-	this.mouse = {
-		click: function(element,f){element.onclick = f;},
-		doubleClick: function(element,f){element.ondblclick = f;},
-		down: function(element,f){element.onmousedown = f;},
-		up: function(element,f){element.onmouseup = f;},
-		move: function(element,f){element.onmousemove = f;},
-		over: function(element,f){element.onmouseover = f;},
-		out: function(element,f){element.onmouseout = f;}
+	this.addEvent = function(element,action,f){
+		if(element.attachEvent){
+			return element.attachEvent('on'+action,f);
+		}else{
+			return element.addEventListener(action,f,false);
+		}
 	}
-	// keyboard events
-	this.key = {
-		down: function(f){window.onkeydown = f;},
-		press: function(f){window.onkeypress = f;},
-		up: function(f){window.onkeyup = f;}
+	this.removeEvent = function(element,action,f){
+		if(element.detachEvent){
+			return element.detachEvent(action,f);
+		}else{
+			return element.removeEventListener(action,f,false);
+		}
 	}
 }
